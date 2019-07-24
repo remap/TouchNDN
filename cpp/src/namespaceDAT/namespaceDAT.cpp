5a3765b1cf91b78e8b9db6331ee5271c1c00e9b2
peetonn <gpeetonn@gmail.com>
2019-07-24 13:52:54 -0700

    setup for namespaceDAT

--- cpp/src/namespaceDAT/namespaceDAT.cpp
+++ cpp/src/namespaceDAT/namespaceDAT.cpp
@@ -18,7 +18,7 @@
  * A copy of the GNU Lesser General Public License is in the file COPYING.
  */
 
-#include "faceDAT.h"
+#include "namespaceDAT.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -26,10 +26,33 @@
 #include <assert.h>
 #include <array>
 
-// These functions are basic C function, which the DLL loader can find
-// much easier than finding a C++ Class.
-// The DLLEXPORT prefix is needed so the compile exports these functions from the .dll
-// you are creating
+#include <cnl-cpp/namespace.hpp>
+
+#define PAR_FACEDAT "Facedat"
+#define PAR_FACEDAT_LABEL "Face DAT"
+#define PAR_KEYCHAINDAT "Keychaindat"
+#define PAR_KEYCHAINDAT_LABEL "KeyChain DAT"
+#define PAR_FRESHNESS "Freshness"
+#define PAR_FRESHNESS_LABEL "Freshness"
+#define PAR_HANDLER_TYPE "Handlertype"
+#define PAR_HANDLER_TYPE_LABEL "Handler"
+#define PAR_TOP_INPUT "Topinput"
+#define PAR_TOP_INPUT_LABEL "Payload TOP"
+
+#define PAR_HANDLER_NONE "Handlernone"
+#define PAR_HANDLER_NONE_LABEL "None"
+#define PAR_HANDLER_SEGMENTED "Handlersegmented"
+#define PAR_HANDLER_SEGMENTED_LABEL "Segmented"
+#define PAR_HANDLER_GOBJ "Handlergobj"
+#define PAR_HANDLER_GOBJ_LABEL "Generalized Object"
+#define PAR_HANDLER_GOSTREAM "Handlergobjstream"
+#define PAR_HANDLER_GOSTREAM_LABEL "Generalized Object Stream"
+#define PAR_OBJECT_NEEDED "Objectneeded"
+#define PAR_OBJECT_NEEDED_LABEL "Object Needed"
+
+using namespace std;
+using namespace touch_ndn;
+
 extern "C"
 {
 
@@ -37,36 +60,20 @@ DLLEXPORT
 void
 FillDATPluginInfo(DAT_PluginInfo *info)
 {
-	// Always return DAT_CPLUSPLUS_API_VERSION in this function.
 	info->apiVersion = DATCPlusPlusAPIVersion;
-
-	// The opType is the unique name for this TOP. It must start with a
-	// capital A-Z character, and all the following characters must lower case
-	// or numbers (a-z, 0-9)
-	info->customOPInfo.opType->setString("Touchndnface");
-
-	// The opLabel is the text that will show up in the OP Create Dialog
-	info->customOPInfo.opLabel->setString("Face DAT");
-
-	// Will be turned into a 3 letter icon on the nodes
-	info->customOPInfo.opIcon->setString("FDT");
-
-	// Information about the author of this OP
+	info->customOPInfo.opType->setString("Touchndnnamespace");
+	info->customOPInfo.opLabel->setString("Namespace DAT");
+	info->customOPInfo.opIcon->setString("NDT");
 	info->customOPInfo.authorName->setString("Peter Gusev");
 	info->customOPInfo.authorEmail->setString("peter@remap.ucla.edu");
-
-	// This DAT works with 0 or 1 inputs
 	info->customOPInfo.minInputs = 0;
 	info->customOPInfo.maxInputs = 1;
-
 }
 
 DLLEXPORT
 DAT_CPlusPlusBase*
 CreateDATInstance(const OP_NodeInfo* info)
 {
-	// Return a new instance of your class every time this is called.
-	// It will be called once per DAT that is using the .dll
 	return new NamespaceDAT(info);
 }
 
@@ -74,23 +81,14 @@ DLLEXPORT
 void
 DestroyDATInstance(DAT_CPlusPlusBase* instance)
 {
-	// Delete the instance here, this will be called when
-	// Touch is shutting down, when the DAT using that instance is deleted, or
-	// if the DAT loads a different DLL
 	delete (NamespaceDAT*)instance;
 }
 
 };
 
-NamespaceDAT::NamespaceDAT(const OP_NodeInfo* info) : myNodeInfo(info)
+NamespaceDAT::NamespaceDAT(const OP_NodeInfo* info)
+: BaseDAT(info)
 {
-	myExecuteCount = 0;
-	myOffset = 0.0;
-
-	myChop = "";
-
-	myChopChanName = "";
-	myChopChanVal = 0;
 }
 
 NamespaceDAT::~NamespaceDAT()
@@ -98,334 +96,79 @@ NamespaceDAT::~NamespaceDAT()
 }
 
 void
-NamespaceDAT::getGeneralInfo(DAT_GeneralInfo* ginfo, const OP_Inputs* inputs, void* reserved1)
-{
-	// This will cause the node to cook every frame
-	ginfo->cookEveryFrameIfAsked = false;
-}
-
-void
-NamespaceDAT::makeTable(DAT_Output* output, int numRows, int numCols)
-{
-	output->setOutputDataType(DAT_OutDataType::Table);
-	output->setTableSize(numRows, numCols);
-
-	std::array<const char*, 5> data = { "this", "is", "some", "test", "data"};
-
-	for (int i = 0; i < numRows; i++)
-	{
-		for (int j = 0; j < numCols; j++)
-		{
-			int j2 = j;
-
-			// If we are asked to make more columns than we have data for
-			if (j2 >= data.size())
-				j2 = j2 % data.size();
-
-			output->setCellString(i, j, data[j2]);
-		}
-	}
-}
-
-void
-NamespaceDAT::makeText(DAT_Output* output)
-{
-	output->setOutputDataType(DAT_OutDataType::Text);
-	output->setText("This is some test data.");
-}
-
-void
 NamespaceDAT::execute(DAT_Output* output,
 							const OP_Inputs* inputs,
 							void* reserved)
 {
-	myExecuteCount++;
-
-	if (!output)
-		return;
-
-	if (inputs->getNumInputs() > 0)
-	{
-		inputs->enablePar("Rows", 0);		// not used
-		inputs->enablePar("Cols", 0);		// not used
-		inputs->enablePar("Outputtype", 0);	// not used
-
-		const OP_DATInput	*cinput = inputs->getInputDAT(0);
-
-		int numRows = cinput->numRows;
-		int numCols = cinput->numCols;
-		bool isTable = cinput->isTable;
-
-		if (!isTable) // is Text
-		{
-			const char* str = cinput->getCell(0, 0);
-			output->setText(str);
-		}
-		else
-		{
-			output->setOutputDataType(DAT_OutDataType::Table);
-			output->setTableSize(numRows, numCols);
-
-			for (int i = 0; i < cinput->numRows; i++)
-			{
-				for (int j = 0; j < cinput->numCols; j++)
-				{
-					const char* str = cinput->getCell(i, j);
-					output->setCellString(i, j, str);
-				}
-			}
-		}
-
-	}
-	else // If no input is connected, lets output a custom table/text DAT
-	{
-		inputs->enablePar("Rows", 1);
-		inputs->enablePar("Cols", 1);
-		inputs->enablePar("Outputtype", 1);
-
-		int outputDataType = inputs->getParInt("Outputtype");
-		int	 numRows = inputs->getParInt("Rows");
-		int	 numCols = inputs->getParInt("Cols");
-
-		switch (outputDataType)
-		{
-			case 0:		// Table
-				makeTable(output, numRows, numCols);
-				break;
-
-			case 1:		// Text
-				makeText(output);
-				break;
-
-			default: // table
-				makeTable(output, numRows, numCols);
-				break;
-		}
-
-		// if there is an input chop parameter:
-		const OP_CHOPInput	*cinput = inputs->getParCHOP("Chop");
-		if (cinput)
-		{
-			int numSamples = cinput->numSamples;
-			int ind = 0;
-			for (int i = 0; i < cinput->numChannels; i++)
-			{
-				myChopChanName = std::string(cinput->getChannelName(i));
-				myChop = inputs->getParString("Chop");
-
-				static char tempBuffer[50];
-				myChopChanVal = float(cinput->getChannelData(i)[ind]);
-
-#ifdef _WIN32
-				sprintf_s(tempBuffer, "%g", myChopChanVal);
-#else // macOS
-				snprintf(tempBuffer, sizeof(tempBuffer), "%g", myChopChanVal);
-#endif
-				if (numCols == 0)
-					numCols = 2;
-				output->setTableSize(numRows + i + 1, numCols);
-				output->setCellString(numRows + i, 0, myChopChanName.c_str());
-				output->setCellString(numRows + i, 1, &tempBuffer[0]);
-			}
-
-		}
-
-	}
-}
-
-int32_t
-NamespaceDAT::getNumInfoCHOPChans(void* reserved1)
-{
-	// We return the number of channel we want to output to any Info CHOP
-	// connected to the CHOP. In this example we are just going to send one channel.
-	return 4;
-}
-
-void
-NamespaceDAT::getInfoCHOPChan(int32_t index,
-									OP_InfoCHOPChan* chan, void* reserved1)
-{
-	// This function will be called once for each channel we said we'd want to return
-	// In this example it'll only be called once.
-
-	if (index == 0)
-	{
-		chan->name->setString("executeCount");
-		chan->value = (float)myExecuteCount;
-	}
-
-	if (index == 1)
-	{
-		chan->name->setString("offset");
-		chan->value = (float)myOffset;
-	}
-
-	if (index == 2)
-	{
-		chan->name->setString(myChop.c_str());
-		chan->value = (float)myOffset;
-	}
-
-	if (index == 3)
-	{
-		chan->name->setString(myChopChanName.c_str());
-		chan->value = myChopChanVal;
-	}
-}
-
-bool
-NamespaceDAT::getInfoDATSize(OP_InfoDATSize* infoSize, void* reserved1)
-{
-	infoSize->rows = 3;
-	infoSize->cols = 3;
-	// Setting this to false means we'll be assigning values to the table
-	// one row at a time. True means we'll do it one column at a time.
-	infoSize->byColumn = false;
-	return true;
-}
-
-void
-NamespaceDAT::getInfoDATEntries(int32_t index,
-									int32_t nEntries,
-									OP_InfoDATEntries* entries,
-									void* reserved1)
-{
-	char tempBuffer[4096];
-
-	if (index == 0)
-	{
-		// Set the value for the first column
-#ifdef _WIN32
-		strcpy_s(tempBuffer, "executeCount");
-#else // macOS
-		strlcpy(tempBuffer, "executeCount", sizeof(tempBuffer));
-#endif
-		entries->values[0]->setString(tempBuffer);
-
-		// Set the value for the second column
-#ifdef _WIN32
-		sprintf_s(tempBuffer, "%d", myExecuteCount);
-#else // macOS
-		snprintf(tempBuffer, sizeof(tempBuffer), "%d", myExecuteCount);
-#endif
-		entries->values[1]->setString(tempBuffer);
-	}
-
-	if (index == 1)
-	{
-		// Set the value for the first column
-#ifdef _WIN32
-		strcpy_s(tempBuffer, "offset");
-#else // macOS
-		strlcpy(tempBuffer, "offset", sizeof(tempBuffer));
-#endif
-		entries->values[0]->setString(tempBuffer);
-
-		// Set the value for the second column
-#ifdef _WIN32
-		sprintf_s(tempBuffer, "%g", myOffset);
-#else // macOS
-		snprintf(tempBuffer, sizeof(tempBuffer), "%g", myOffset);
-#endif
-		entries->values[1]->setString(tempBuffer);
-	}
-
-	if (index == 2)
-	{
-		// Set the value for the first column
-#ifdef _WIN32
-		strcpy_s(tempBuffer, "DAT input name");
-#else // macOS
-		strlcpy(tempBuffer, "offset", sizeof(tempBuffer));
-#endif
-		entries->values[0]->setString(tempBuffer);
-
-		// Set the value for the second column
-#ifdef _WIN32
-		strcpy_s(tempBuffer, myDat.c_str());
-#else // macOS
-		snprintf(tempBuffer, sizeof(tempBuffer), "%g", myOffset);
-#endif
-		entries->values[1]->setString(tempBuffer);
-	}
+    BaseDAT::execute(output, inputs, reserved);
 }
 
 void
 NamespaceDAT::setupParameters(OP_ParameterManager* manager, void* reserved1)
 {
-	// CHOP
-	{
-		OP_StringParameter	np;
-
-		np.name = "Chop";
-		np.label = "CHOP";
-
-		OP_ParAppendResult res = manager->appendCHOP(np);
-		assert(res == OP_ParAppendResult::Success);
-	}
-
-	// Number of Rows
-	{
-		OP_NumericParameter	np;
-
-		np.name = "Rows";
-		np.label = "Rows";
-		np.defaultValues[0] = 4;
-		np.minSliders[0] = 0;
-		np.maxSliders[0] = 10;
-
-		OP_ParAppendResult res = manager->appendInt(np);
-		assert(res == OP_ParAppendResult::Success);
-	}
-
-	// Number of Columns
-	{
-		OP_NumericParameter	np;
-
-		np.name = "Cols";
-		np.label = "Cols";
-		np.defaultValues[0] = 5;
-		np.minSliders[0] = 0;
-		np.maxSliders[0] = 10;
-
-		OP_ParAppendResult res = manager->appendInt(np);
-		assert(res == OP_ParAppendResult::Success);
-	}
-
-	// DAT output type
-	{
-		OP_StringParameter	sp;
-
-		sp.name = "Outputtype";
-		sp.label = "Output Type";
-
-		sp.defaultValue = "Table";
-
-		const char *names[] = {"Table", "Text"};
-		const char *labels[] = {"Table", "Text"};
-
-		OP_ParAppendResult res = manager->appendMenu(sp, 2, names, labels);
-		assert(res == OP_ParAppendResult::Success);
-	}
-
-	// pulse
-	{
-		OP_NumericParameter	np;
-
-		np.name = "Reset";
-		np.label = "Reset";
-
-		OP_ParAppendResult res = manager->appendPulse(np);
-		assert(res == OP_ParAppendResult::Success);
-	}
-
+    BaseDAT::setupParameters(manager, reserved1);
+    
+    appendPar<OP_StringParameter>
+    (manager, PAR_FACEDAT, PAR_FACEDAT_LABEL, PAR_PAGE_DEFAULT,
+     [&](OP_StringParameter &p){
+         return manager->appendDAT(p);
+     });
+    appendPar<OP_StringParameter>
+    (manager, PAR_KEYCHAINDAT, PAR_KEYCHAINDAT_LABEL, PAR_PAGE_DEFAULT,
+     [&](OP_StringParameter &p){
+         return manager->appendDAT(p);
+     });
+    appendPar<OP_NumericParameter>
+    (manager, PAR_FRESHNESS, PAR_FRESHNESS_LABEL, PAR_PAGE_DEFAULT,
+     [&](OP_NumericParameter &p){
+         p.minValues[0] = 0;
+         p.maxValues[0] = 24*3600*1000;
+         p.minSliders[0] = p.minValues[0];
+         p.maxSliders[0] = p.maxValues[0];
+         p.defaultValues[0] = freshness_;
+         return manager->appendInt(p);
+     });
+    
+#define PAR_HANDLER_MENU_SIZE 4
+    static const char *names[PAR_HANDLER_MENU_SIZE] = {
+        PAR_HANDLER_NONE,
+        PAR_HANDLER_SEGMENTED,
+        PAR_HANDLER_GOBJ,
+        PAR_HANDLER_GOSTREAM
+    };
+    static const char *labels[PAR_HANDLER_MENU_SIZE] = {
+        PAR_HANDLER_NONE_LABEL,
+        PAR_HANDLER_SEGMENTED_LABEL,
+        PAR_HANDLER_GOBJ_LABEL,
+        PAR_HANDLER_GOSTREAM_LABEL
+    };
+    
+    appendPar<OP_StringParameter>
+    (manager, PAR_HANDLER_TYPE, PAR_HANDLER_TYPE_LABEL, PAR_PAGE_DEFAULT,
+     [&](OP_StringParameter &p){
+         return manager->appendMenu(p, PAR_HANDLER_MENU_SIZE, names, labels);
+     });
+    
+    appendPar<OP_NumericParameter>
+    (manager, PAR_OBJECT_NEEDED, PAR_OBJECT_NEEDED_LABEL, PAR_PAGE_DEFAULT,
+     [&](OP_NumericParameter &p){
+         return manager->appendPulse(p);
+     });
+    
+    appendPar<OP_StringParameter>
+    (manager, PAR_TOP_INPUT, PAR_TOP_INPUT_LABEL, PAR_PAGE_DEFAULT,
+     [&](OP_StringParameter &p){
+         return manager->appendDAT(p);
+     });
 }
 
 void
 NamespaceDAT::pulsePressed(const char* name, void* reserved1)
 {
-	if (!strcmp(name, "Reset"))
-	{
-		myOffset = 0.0;
-	}
+    if (string(name) == PAR_OBJECT_NEEDED)
+    {
+        cout << "object needed " << endl;
+    }
+    else
+        BaseDAT::pulsePressed(name, reserved1);
 }
--- cpp/src/namespaceDAT/namespaceDAT.h
+++ cpp/src/namespaceDAT/namespaceDAT.h
@@ -18,63 +18,48 @@
  * A copy of the GNU Lesser General Public License is in the file COPYING.
  */
 
-
-#include "DAT_CPlusPlusBase.h"
 #include <string>
+
+#include "baseDAT.hpp"
+
+namespace cnl_cpp {
+    class Namespace;
+}
+
+namespace touch_ndn
+{
 
 /*
  This is a basic sample project to represent the usage of CPlusPlus DAT API.
  To get more help about these functions, look at DAT_CPlusPlusBase.h
 */
-
-class NamespaceDAT : public DAT_CPlusPlusBase
+class NamespaceDAT : public BaseDAT
 {
 public:
 	NamespaceDAT(const OP_NodeInfo* info);
 	virtual ~NamespaceDAT();
-
-	virtual void		getGeneralInfo(DAT_GeneralInfo*, const OP_Inputs*, void* reserved1) override;
 
 	virtual void		execute(DAT_Output*,
 								const OP_Inputs*,
 								void* reserved) override;
 
 
-	virtual int32_t		getNumInfoCHOPChans(void* reserved1) override;
-	virtual void		getInfoCHOPChan(int index,
-										OP_InfoCHOPChan* chan, 
-										void* reserved1) override;
-
-	virtual bool		getInfoDATSize(OP_InfoDATSize* infoSize, void* reserved1) override;
-	virtual void		getInfoDATEntries(int32_t index,
-											int32_t nEntries,
-											OP_InfoDATEntries* entries,
-											void* reserved1) override;
+//    virtual int32_t        getNumInfoCHOPChans(void* reserved1) override;
+//    virtual void        getInfoCHOPChan(int index,
+//                                        OP_InfoCHOPChan* chan, 
+//                                        void* reserved1) override;
+//
+//    virtual bool        getInfoDATSize(OP_InfoDATSize* infoSize, void* reserved1) override;
+//    virtual void        getInfoDATEntries(int32_t index,
+//                                            int32_t nEntries,
+//                                            OP_InfoDATEntries* entries,
+//                                            void* reserved1) override;
 
 	virtual void		setupParameters(OP_ParameterManager* manager, void* reserved1) override;
 	virtual void		pulsePressed(const char* name, void* reserved1) override;
 
 private:
-
-	void				makeTable(DAT_Output* output, int numRows, int numCols);
-	void				makeText(DAT_Output* output);
-
-	// We don't need to store this pointer, but we do for the example.
-	// The OP_NodeInfo class store information about the node that's using
-	// this instance of the class (like its name).
-	const OP_NodeInfo*	myNodeInfo;
-
-	// In this example this value will be incremented each time the execute()
-	// function is called, then passes back to the DAT
-	int32_t				myExecuteCount;
-
-	double				myOffset;
-
-	std::string         myChopChanName;
-	float               myChopChanVal;
-	std::string         myChop;
-
-	std::string         myDat;
-
-
+    uint32_t freshness_;
 };
+
+}
